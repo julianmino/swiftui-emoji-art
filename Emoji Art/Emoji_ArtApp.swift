e5899afcf64f618f7f11d16d2c68969e567e6e6d
Julián Miño <m99_julian@hotmail.com>
2024-01-14 15:14:11 -0300

    Initial Commit

---
+++ Emoji Art/Emoji_ArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji_ArtApp.swift
+//  Emoji Art
+//
+//  Created by Julian Miño on 14/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Emoji_ArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
